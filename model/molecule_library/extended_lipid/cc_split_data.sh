c94096465d05f895a3ada79f9e99b30f26dcad78
Kuan Pang <kuanpang@stanford.edu>
2025-04-10 16:53:50 -0700

    reorganzie the file and add instructions

--- model/molecule_library/extended_lipid/cc_split_data.sh
+++
@@ -1,8 +0,0 @@
-module load StdEnv/2020
-module unload python
-
-
-
-
-cd /home/pangkuan/projects/def-bowenli/pangkuan/SDL-LNP/model/molecule_library/extended_lipid
-~/miniconda3/envs/unimol/bin/python split_data.py
--- opentron/simulate.sh
+++
@@ -1,6 +0,0 @@
-# shellcheck disable=SC2164
-
-#print pwd
-echo "Current directory: $(pwd)"
-cd s1_lipid_synthesis
-opentrons_simulate -e lipid_synthesis.py
